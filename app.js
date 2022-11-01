a27d27f8d9624c60b7a2470a7f4f112cd6d0f469
IMatfres <matt.estunlp@gmail.com>
2022-11-01 12:06:37 -0300

    add bd

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((err, req, res) => {
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Servidor funcionando en el puerto ${port}`)
+  console.log(`Servidor funcionando en el puerto http://localhost:${port}`)
 })
 
 module.exports = app
