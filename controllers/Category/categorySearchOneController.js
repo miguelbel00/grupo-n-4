07318d9e41343e96b292edeae6bd18d95ead6c54
IMatfres <matt.estunlp@gmail.com>
2022-11-03 12:08:00 -0300

    feat(controllers/Category): add endpoint GET /categories/:id

--- controllers/Category/categorySearchOneController.js
+++ controllers/Category/categorySearchOneController.js
@@ -1,13 +1,14 @@
 const createHttpError = require('http-errors')
 const { Category } = require('../../database/models')
 const { endpointResponse } = require('../../helpers/success')
+const {ErrorObject } = require('../../helpers/error')
 const { catchAsync } = require('../../helpers/catchAsync')
 
 module.exports = {
     getCategoryById: catchAsync(async (req, res, next) => {
       try {
         const response = await Category.findOne({ where: { id: req.params.id } });
-        if(!response) throw new Error('Category, does not exist')
+        if(!response) throw new ErrorObject('Category not found', 404)
         endpointResponse({
           res,
           message: 'Category retrieved successfully',
