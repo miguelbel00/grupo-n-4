2f1aeff30bc7fb0c6fa0ed7a60b45a9815dfb944
Ana Kloss <anak492@gmail.com>
2022-11-03 20:38:16 -0300

    refactor(controller/User): modified createUser

--- controllers/Users/createUserControllers.js
+++ controllers/Users/createUserControllers.js
@@ -1,12 +1,47 @@
 const createHttpError = require('http-errors')
+const bcrypt = require('bcrypt');
 const { endpointResponse } = require('../../helpers/success')
+const { ErrorObject } = require('../../helpers/error');
 const { catchAsync } = require('../../helpers/catchAsync')
-const { createUser } = require('../../services/users');
+const { User } = require('../../database/models');
+
+// module.exports = {
+//   create: catchAsync(async (req, res, next) => {
+//     try {
+//       const newUser = await createUser(req.body)
+//       endpointResponse({
+//         res,
+//         message: 'User created',
+//         body: newUser,
+//       })
+
+//     } catch (error) {
+//       const httpError = createHttpError(
+//         error.statusCode,
+//         `[Error creating User] - [createUserControllers - POST]: ${ error.message }`,
+//       )
+//       next(httpError)
+//     }
+//   }),
+// }
 
 module.exports = {
-  create: catchAsync(async (req, res, next) => {
+  createUser: catchAsync(async (req, res, next) => {
     try {
-      const newUser = createUser(req.body);
+
+      // verificate (ERROR)
+      const existentUser = await User.findOne({ where: { email: req.body.email } })
+      if (existentUser) throw new ErrorObject('That email is already in use', 404)
+
+      // encrypt pass
+      const hashedPass = await bcrypt.hash(req.body.password, 10);
+      req.body.password = hashedPass;
+      req.body.roleId = 1;
+
+      // create user
+      const newUser = await User.create(req.body);
+      if (!newUser) throw new ErrorObject('User creation failed', 404)
+      
       endpointResponse({
         res,
         message: 'User created',
@@ -20,5 +55,5 @@ module.exports = {
       )
       next(httpError)
     }
-  }),
+  })
 }
