51706e722dd626391a83a488a731cd1e34b35d9d
didierrevelo <didierrevelo@gmail.com>
2022-11-03 17:20:27 -0500

    created validator for category

---
+++ schemas/categoryValidatorSchema.js
@@ -0,0 +1,29 @@
+const { checkSchema } = require('express-validator');
+
+
+/* A validation schema for the category model. */
+const CategorySchema = checkSchema(
+  {
+  name: {
+    isLength: {
+      errorMessage: 'Name should be at least 5 chars long',
+      options: { min: 5, max: 20},
+    },
+    exists: {
+      errorMessage: 'Name is required',
+    },
+  },
+  description: {
+    isLength: {
+      errorMessage: 'Description should be at least 5 chars long',
+      options: { min: 5, max:50 },
+    },
+    exists: {
+      errorMessage: 'Description is required',
+    },
+  },
+});
+
+module.exports = {
+  CategorySchema,
+};
