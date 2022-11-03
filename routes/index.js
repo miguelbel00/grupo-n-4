8181455f07d4678a409e5cf04aa31c268c2e0006
IMatfres <matt.estunlp@gmail.com>
2022-11-03 10:18:09 -0300

    feat(controllers/Categoryr): add endpoint POST /categories

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const { get } = require('../controllers/index')
-
+const categoryRoutes = require('./categoryRoutes')
 const router = express.Router()
 
-// example of a route with index controller get function
-router.get('/', get)
+router.use('/', categoryRoutes)
+
 
 module.exports = router
