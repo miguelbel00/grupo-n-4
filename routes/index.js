1a7ab0418b5a75b8292932663b2e7a500aaeec06
didierrevelo <didierrevelo@gmail.com>
2022-11-03 10:28:29 -0500

    Testing post route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { get } = require('../controllers/index')
+const { get, post } = require('../controllers/index')
+const { TestsSchema } = require('../schemas/testsValidatorSchema')
+const validatorSchemas = require('../middlewares/validatorSchemas')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
+router.post('/test', validatorSchemas(TestsSchema), post)
 
 module.exports = router
