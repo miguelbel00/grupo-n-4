211d30e82a4b31a6181e99d9f84a8dc6176a1056
sasuske <jelver00@hotmail.com>
2022-11-03 11:17:05 -0500

    config database

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql'
+    dialect: 'postgres'
   },
 
   production: {
