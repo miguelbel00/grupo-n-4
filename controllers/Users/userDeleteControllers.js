f345f324474c7f2a06f58ae19cbe676a7878bc61
unknown <fernandoopaez@gmail.com>
2022-11-04 14:53:23 -0300

    userDeleteControllers fixed A

--- controllers/Users/userDeleteControllers.js
+++ controllers/Users/userDeleteControllers.js
@@ -8,9 +8,9 @@ const { User } = require("../../database/models");
 module.exports = {
     deleteUser: catchAsync(async (req, res, next) => {
         try {
-            const { id } = req.params;    
+            const { id } = req.params;
             const user = await User.findByPk(id);
-    
+
             if (user != null) {
                 const responce = await User.destroy({
                     where: { id }
@@ -19,19 +19,18 @@ module.exports = {
                     res,
                     message: "User successfully deleted",
                     body: responce,
-                  });
+                });
             } else {
                 throw next(new ErrorObject(" Usuario not found", 400));
             }
-    
+
         } catch (error) {
             const httpError = createHttpError(
                 error.statusCode,
                 `[Error deleted used] - [User/userDeleteControllers.js - DELETE]: ${error.message}`
-              );
-              next(httpError);
+            );
+            next(httpError);
         }
 
     })
-  };
-  
+};
