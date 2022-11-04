c40f63d332248ce8e6efdb4997b7f2f2431de7e7
sasuske <jelver00@hotmail.com>
2022-11-03 19:54:12 -0500

    conflict resolved

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const { updateUser } = require("../controllers/Users/userUpdateControllers");
-const { getById } = require("../controllers/Users/userByIdController");
+const { getById } = require("../controllers/Users/userByIdcontrollers");
 
 const router = Router();
 
+router.get("/users/:id", getById);
 router.put("/users/:id", updateUser);
 
 module.exports = router;
