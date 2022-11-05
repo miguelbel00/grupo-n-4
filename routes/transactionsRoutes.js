d075c9b9d51c8aedeb5f87b9fe5ff201f459c812
IMatfres <matt.estunlp@gmail.com>
2022-11-05 13:16:31 -0300

    feat(controllers/Transactions): add endpoint POST /transactions

--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const { createTransaction } = require('../controllers/Transactions/transactionCreateController');
 const { getAllTransactions } = require('../controllers/Transactions/transactionSearchController')
 const { getTransactionById } = require('../controllers/Transactions/transactionsSearchOneController');
 const router = express.Router()
 
+router.post('/transactions', createTransaction)
 router.get('/transactions', getAllTransactions)
 router.get('/transactions/:id', getTransactionById)
 
