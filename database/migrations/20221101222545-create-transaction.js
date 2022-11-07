ad4c25dcd7206602f6b322a41df7b9472bcbbfa2
unknown <fernandoopaez@gmail.com>
2022-11-07 16:48:38 -0300

    add deleteAt field to /migrations/transactions

--- database/migrations/20221101222545-create-transaction.js
+++ database/migrations/20221101222545-create-transaction.js
@@ -18,7 +18,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: 'Users',
-          key:'id'
+          key: 'id'
         },
         onDelet: 'CASCADE',
         onUpdate: 'CASCADE'
@@ -27,7 +27,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: {
           model: 'Categories',
-          key:'id'
+          key: 'id'
         },
         onDelet: 'CASCADE',
         onUpdate: 'CASCADE'
@@ -42,7 +42,11 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
       }
+
     });
   },
   down: async (queryInterface, Sequelize) => {
