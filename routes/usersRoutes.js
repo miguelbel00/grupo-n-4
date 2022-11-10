19637611625ac0ac52ceda9286fccfe93bf44b19
unknown <miguel00@hotmail.com>
2022-11-10 14:28:03 -0500

    fix(auth) change create user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const { updateUser } = require('../controllers/Users/userUpdateControllers');
-const { createUser } = require('../controllers/Users/userCreateControllers');
 const { getById } = require("../controllers/Users/userByIdcontrollers");
 const { deleteUser } = require("../controllers/Users/userDeleteControllers");
 const { getAllUsers } = require('../controllers/Users/userSearchController')
@@ -9,10 +8,8 @@ const {imageUpload} = require("../controllers/Users/imageUploadControllers");
 
 const router = Router();
 
-router.post('/users', createUser);
 router.put('/users/:id', updateUser)
 router.get("/users/:id", getById);
-router.put("/users/:id", updateUser);
 router.delete("/users/:id",deleteUser);
 router.get('/users', getAllUsers)
 router.post('/image',upload.single('image'),imageUpload)
