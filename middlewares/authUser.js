5263aba95fbca5be776f5e23ef750bd5c6fb23c2
unknown <fernandoopaez@gmail.com>
2022-11-10 15:55:02 -0300

    proved jwt verify method

---
+++ middlewares/authUser.js
@@ -0,0 +1,15 @@
+const createHttpError = require("http-errors");
+const { catchAsync } = require("../helpers/catchAsync");
+const { ErrorObject } = require("../helpers/error");
+const jwt = require("jsonwebtoken");
+const {User}=require ('../database/models');
+
+module.exports={
+   
+   
+
+    }
+    
+
+
+
