e1322ea18e778209d2ad9ed3d2c1432c25391191
Ana Kloss <anak492@gmail.com>
2022-11-11 11:54:11 -0300

    fix(ownership/middleware): change verificate route

--- middlewares/ownership.js
+++ middlewares/ownership.js
@@ -1,19 +1,12 @@
 const createHttpError = require('http-errors')
-const { decodificate } = require('./JWT')
-const { User } = require('../database/models')
 const { catchAsync } = require('../helpers/catchAsync');
 const { ErrorObject } = require("../helpers/error");
 
 module.exports = {
-  Ownership: catchAsync(async (req, res, next) => {
+  ownership: catchAsync(async (req, res, next) => {
     try {
-      const token = req.headers['x-access-token'];
-      if (!token) throw new ErrorObject('No token provided', 404);
 
-      const decoded = decodificate(token);
-      const user = await User.findByPk(decoded.id, { password: 0 });
-
-      if (decoded.id !== parseInt(req.params.id) && user.roleId !== 1)
+      if (req.user.id !== parseInt(req.params.id) && req.user.roleId !== 1)
         throw new ErrorObject('You dont have permission to access', 403);
 
       next();
