b9af3de6df0863c597c0a331493a834e5e0311cb
IMatfres <matt.estunlp@gmail.com>
2022-11-11 17:43:08 -0300

    feat(swagger): add documentation for categories

--- middlewares/JWT.js
+++ middlewares/JWT.js
@@ -1,46 +1,40 @@
-const { request, response } = require('express');
 const jwt = require('jsonwebtoken');
-const { Role } = require('../database/models/');
-const { User } = require('../database/models/');
 const { ErrorObject } = require('../helpers/error');
+const dotenv = require('dotenv');
+const createHttpError = require('http-errors');
+dotenv.config()
 
-const codificate = (data) => {
+const encode = (data) => {
     const token = jwt.sign(data, process.env.SECRETORPRIVATEKEY)
     return token
 }
 
-const decodificate = (token) => {
-    const payload = jwt.decodificate(token)
+const decode = (token) => {
+    const payload = jwt.decode(token)
     return payload;
 }
 
-const verify = async (req = request, res = response, next) => {
-    const { authorization } = req.headers;
-
-    if (!authorization || !authorization.startsWith("Bearer")) {
-        throw new ErrorObject("No token provided", 403);
-    }
-
+const verify = (req, res, next) => {
     try {
-
-        const token = authorization.split(' ')[1]
-        const { id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
-
-        const { dataValues: user } = await User.findOne({ where: { id } });
-
-        if (!user) {
-            throw new ErrorObject("The owner of this token does not exist anymore", 403);
+        const { authorization } = req.headers;
+        if (!authorization) {
+            throw new ErrorObject("Token is required", 403);
         }
-
-        req.user = user;
+        const token = authorization.split(' ')[1]
+        const payload = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
         next()
     } catch (error) {
-        throw new ErrorObject('Your session has expired. Please relogin', 400)
+        const httpError = createHttpError(
+            error.statusCode,
+            `[Error verify token] - [JWT - Middleware]: ${error.message}`
+        );
+        next(httpError);
+
     }
 }
 
 module.exports = {
-    codificate,
-    decodificate,
+    encode,
+    decode,
     verify
 }
