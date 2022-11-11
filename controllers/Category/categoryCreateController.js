8bf19f0939c2c2f8fc8d8ed34c594128e066be05
unknown <miguel00@hotmail.com>
2022-11-11 14:19:05 -0500

    feat(test): create category test

--- controllers/Category/categoryCreateController.js
+++ controllers/Category/categoryCreateController.js
@@ -1,26 +1,28 @@
-const createHttpError = require('http-errors')
-const { Category } = require('../../database/models')
-const { endpointResponse } = require('../../helpers/success')
-const { catchAsync } = require('../../helpers/catchAsync')
+const createHttpError = require("http-errors");
+const { Category } = require("../../database/models");
+const { endpointResponse } = require("../../helpers/success");
+const { catchAsync } = require("../../helpers/catchAsync");
+const { ErrorObject } = require("../../helpers/error");
 
 module.exports = {
-    createCategory: catchAsync(async (req, res, next) => {
-      try {
-        const response = await Category.create({
-            name: req.body.name,
-            description: req.body.description
-          })
-        endpointResponse({
-          res,
-          message: 'Category created',
-          body: response,
-        })
-      } catch (error) {
-        const httpError = createHttpError(
-          error.statusCode,
-          `[Error create category] - [categoryCreateController - POST]: ${error.message}`,
-        )
-        next(httpError)
+  createCategory: catchAsync(async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
+      if (typeof name !== "string" || typeof description !== "string") {
+        throw new ErrorObject("missing or wrong parameters", 404);
       }
-    }),
-  }
+      const response = await Category.create({name,description});
+      endpointResponse({
+        res,
+        message: "Category created",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error create category] - [categoryCreateController - POST]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
--- controllers/Category/categoryUpdateController.js
+++ controllers/Category/categoryUpdateController.js
@@ -2,16 +2,17 @@ const createHttpError = require('http-errors')
 const { Category } = require('../../database/models')
 const { endpointResponse } = require('../../helpers/success')
 const { catchAsync } = require('../../helpers/catchAsync')
+const { ErrorObject } = require('../../helpers/error')
 
 module.exports = {
     updateCategory: catchAsync(async (req, res, next) => {
       try {
-        const data = req.body
-        const response = await Category.update(data,{
-          where: {
-            id: req.params.id 
-          }
-        })
+        const{ id} = req.params
+        const {description,name} = req.body
+        if (!id || !description || !name) throw new ErrorObject('Missing or wrong parameters',404)
+        const categoryFound = await Category.findByPk(id)
+        if (categoryFound==null) throw new ErrorObject('Category not found',404)
+        const response = await categoryFound.update({description,name})
         endpointResponse({
           res,
           message: 'Category Update',
