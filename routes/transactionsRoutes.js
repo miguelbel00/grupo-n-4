3228571d35e1a4c38a06bfda1c8c31db2f2614c4
unknown <fernandoopaez@gmail.com>
2022-11-11 13:19:05 -0300

    add middleware  verify and verifyUser to transactionsRoutes

--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -15,13 +15,15 @@ const {
 const {
   deleteTransaction,
 } = require("../controllers/Transactions/transactionsDeleteControllers");
+const{verify}= require("../middlewares/JWT");
+const{verifyUser}=require("../middlewares/verifyUser");
 
 const router = express.Router();
 
-router.put("/transactions/:id", updateTransactionById);
-router.post("/transactions", createTransaction);
-router.get("/transactions", listTransactions);
-router.get("/transactions/:id", getTransactionById);
-router.delete("/transactions/:id", deleteTransaction);
+router.put("/transactions/:id",[verify, verifyUser], updateTransactionById);
+router.post("/transactions",[verify, verifyUser], createTransaction);
+router.get("/transactions",[verify, verifyUser], listTransactions);
+router.get("/transactions/:id",[verify, verifyUser], getTransactionById);
+router.delete("/transactions/:id",[verify, verifyUser], deleteTransaction);
 
 module.exports = router;
