5bfbce64692f05d87b41ba643bccfb5c966a3efb
Ana Kloss <anak492@gmail.com>
2022-11-10 21:39:34 -0300

    refactor(ownership/middleware)

---
+++ middlewares/ownership.js
@@ -0,0 +1,34 @@
+const createHttpError = require('http-errors')
+const { decodificate } = require('./JWT')
+const { User } = require('../database/models')
+const { catchAsync } = require('../helpers/catchAsync');
+const { endpointResponse } = require("../helpers/success");
+const { ErrorObject } = require("../helpers/error");
+
+module.exports = {
+  Ownership: catchAsync(async (req, res, next) => {
+    try {
+      const token = req.headers['x-access-token'];
+      if (!token) throw new ErrorObject('No token provided', 404);
+
+      const decoded = decodificate(token);
+      const user = await User.findByPk(decoded.id, { password: 0 });
+
+      if (decoded.id !== parseInt(req.params.id) && user.roleId !== 1)
+        throw new ErrorObject('You dont have permission to access', 403);
+
+      next();
+      // endpointResponse({
+      //   res,
+      //   message: "ownership successfully",
+      //   body: { user, token },
+      // });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error ownership] - [updateUserController - PUT]: ${error.message}`
+      );
+      next(httpError);
+    }
+  })
+}
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,17 +2,18 @@ const { Router } = require("express");
 const { updateUser } = require('../controllers/Users/userUpdateControllers');
 const { getById } = require("../controllers/Users/userByIdcontrollers");
 const { deleteUser } = require("../controllers/Users/userDeleteControllers");
-const { getAllUsers } = require('../controllers/Users/userSearchController')
-const {upload} = require('../helpers/imageService');
-const {imageUpload} = require("../controllers/Users/imageUploadControllers");
+const { getAllUsers } = require('../controllers/Users/userSearchController');
+const { upload } = require('../helpers/imageService');
+const { imageUpload } = require("../controllers/Users/imageUploadControllers");
+const { Ownership } = require('../middlewares/ownership');
 
 const router = Router();
 
-router.put('/users/:id', updateUser)
+router.put('/users/:id', Ownership, updateUser)
 router.get("/users/:id", getById);
-router.delete("/users/:id",deleteUser);
+router.delete("/users/:id", deleteUser);
 router.get('/users', getAllUsers)
-router.post('/image',upload.single('image'),imageUpload)
+router.post('/image', upload.single('image'), imageUpload)
 
 module.exports = router;
 
