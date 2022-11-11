54b5c7c6a3780bed7b6bba085364df814d59dd2b
unknown <fernandoopaez@gmail.com>
2022-11-11 11:29:40 -0300

    add validateUser file

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,7 +5,7 @@ const { deleteUser } = require("../controllers/Users/userDeleteControllers");
 const { getAllUsers } = require('../controllers/Users/userSearchController')
 const {upload} = require('../helpers/imageService');
 const {imageUpload} = require("../controllers/Users/imageUploadControllers");
-
+const{verify}= require("../middlewares/JWT")
 const router = Router();
 
 router.put('/users/:id', updateUser)
